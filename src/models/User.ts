d6385f4f78d658e3a223d56e18011325395df622
Tashila <tashila.dev@gmail.com>
2022-10-19 21:33:44 +0400

    Added in save functionality

--- src/models/User.ts
+++ src/models/User.ts
@@ -40,9 +40,19 @@ export class User {
   }
 
   fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      })
+    axios.get(`http://localhost:3000/users/${this.get('id')}`).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+  }
+
+  save(): void {
+    const id = this.get('id');
+    if (id) {
+      // put
+      axios.put(`http://localhost:3000/users/${id}`, this.data);
+    } else {
+      // post
+      axios.post('http://localhost:3000/users', this.data);
+    }
   }
 }
