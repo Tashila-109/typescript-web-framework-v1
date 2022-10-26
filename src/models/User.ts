037a8ea3f03c4b98e35e5646cf6c1b03c0ef3393
Tashila <tashila.dev@gmail.com>
2022-10-26 17:59:38 +0400

    Connected sync back to user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,6 @@
+import { Sync } from './Sync';
 import { Eventing } from './Eventing';
+
 
 export interface UserProps {
   id?: number;
@@ -6,8 +8,11 @@ export interface UserProps {
   age?: number;
 }
 
+const rootUrl = 'http://localhost:3000/users';
+
 export class User {
   public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
 
   constructor(private data: UserProps) {}
 
